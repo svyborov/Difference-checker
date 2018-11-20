a805515fe24eba22ea8e0b5f426c3216b8598427
troyanec <sergey-vyborov@yandex.ru>
2018-11-20 21:45:51 +0300

    fix absolute path test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -5,10 +5,11 @@ import genDiff from '../src/genDiff';
 
 const path1 = './__tests__/__fixtures__/before.json';
 const path2 = './__tests__/__fixtures__/after.json';
+const path3 = './__tests__/__fixtures__/diff.json';
 test('relativePath', () => {
   expect(genDiff(path1, path2)).toBe(fs.readFileSync('./__tests__/__fixtures__/diff.json').toString());
 });
 
 test('absolutePath', () => {
-  expect(genDiff(resolve(path1), resolve(path2))).toBe(fs.readFileSync('/home/troyanec/projects/gendiff/__tests__/__fixtures__/diff.json').toString());
+  expect(genDiff(resolve(path1), resolve(path2))).toBe(fs.readFileSync(resolve(path3)).toString());
 });
