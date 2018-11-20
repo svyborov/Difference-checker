56d12efe4cf47250f83858fdb9f833492b5345cc
troyanec <sergey-vyborov@yandex.ru>
2018-11-21 00:13:23 +0300

    add new test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,15 +1,10 @@
 import fs from 'fs';
-import { resolve } from 'path';
 import genDiff from '../src/genDiff';
 
-
-const path1 = './__tests__/__fixtures__/before.json';
-const path2 = './__tests__/__fixtures__/after.json';
-const path3 = './__tests__/__fixtures__/diff.json';
-test('relativePath', () => {
-  expect(genDiff(path1, path2)).toBe(fs.readFileSync('./__tests__/__fixtures__/diff.json').toString());
+test('path', () => {
+  expect(genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json')).toBe(fs.readFileSync('./__tests__/__fixtures__/diff.json').toString());
 });
 
-test('absolutePath', () => {
-  expect(genDiff(resolve(path1), resolve(path2))).toBe(fs.readFileSync(resolve(path3)).toString());
+test('newPath', () => {
+  expect(genDiff('./__tests__/__fixtures__/newBefore.json', './__tests__/__fixtures__/newAfter.json')).toBe(fs.readFileSync('./__tests__/__fixtures__/newDiff.json').toString());
 });
