2f877032ede78be193df28f13b26dfaba20e3f4d
troyanec <sergey-vyborov@yandex.ru>
2018-11-21 00:19:58 +0300

    fix test

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -3,8 +3,5 @@ import genDiff from '../src/genDiff';
 
 test('path', () => {
   expect(genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json')).toBe(fs.readFileSync('./__tests__/__fixtures__/diff.json').toString());
-});
-
-test('newPath', () => {
   expect(genDiff('./__tests__/__fixtures__/newBefore.json', './__tests__/__fixtures__/newAfter.json')).toBe(fs.readFileSync('./__tests__/__fixtures__/newDiff.json').toString());
 });
