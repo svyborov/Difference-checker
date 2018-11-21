fc4531b6f7164127af36b56dfd4a10daf7e65930
troyanec <sergey-vyborov@yandex.ru>
2018-11-21 12:54:14 +0300

    refactor

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -1,7 +1,11 @@
 import fs from 'fs';
-import genDiff from '../src/genDiff';
+import genDiff from '../src';
 
 test('path', () => {
-  expect(genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json')).toBe(fs.readFileSync('./__tests__/__fixtures__/diff.json').toString());
-  expect(genDiff('./__tests__/__fixtures__/newBefore.json', './__tests__/__fixtures__/newAfter.json')).toBe(fs.readFileSync('./__tests__/__fixtures__/newDiff.json').toString());
+  const exeptValue1 = genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json');
+  const toBeValue1 = fs.readFileSync('./__tests__/__fixtures__/diff.json').toString();
+  expect(exeptValue1).toBe(toBeValue1);
+  const exeptValue2 = genDiff('./__tests__/__fixtures__/newBefore.json', './__tests__/__fixtures__/newAfter.json');
+  const toBeValue2 = fs.readFileSync('./__tests__/__fixtures__/newDiff.json').toString();
+  expect(exeptValue2).toBe(toBeValue2);
 });
--- src/genDiff.js
+++ src/genDiff.js
@@ -5,23 +5,19 @@ const genDiff = (pathToFile1, pathToFile2) => {
   const parseJsonFromFile1 = JSON.parse(fs.readFileSync(pathToFile1).toString());
   const parseJsonFromFile2 = JSON.parse(fs.readFileSync(pathToFile2).toString());
   const keysFromJsons = Object.keys(parseJsonFromFile1).concat(Object.keys(parseJsonFromFile2));
-  const keysSet = Array.from(new Set(keysFromJsons));
-  const result = keysSet.reduce((acc, key) => {
+  console.log(keysFromJsons);
+  const uniqKeys = _.uniq(keysFromJsons);
+  const result = uniqKeys.reduce((acc, key) => {
     if (_.has(parseJsonFromFile1, key)) {
       if (_.has(parseJsonFromFile2, key)) {
         if (parseJsonFromFile1[key] === parseJsonFromFile2[key]) {
-          acc.push(`  ${key}: ${parseJsonFromFile1[key]}`);
-          return acc;
+          return [...acc, `  ${key}: ${parseJsonFromFile1[key]}`];
         }
-        acc.push(`- ${key}: ${parseJsonFromFile1[key]}`);
-        acc.push(`+ ${key}: ${parseJsonFromFile2[key]}`);
-        return acc;
+        return [...acc, `- ${key}: ${parseJsonFromFile1[key]}`, `+ ${key}: ${parseJsonFromFile2[key]}`];
       }
-      acc.push(`- ${key}: ${parseJsonFromFile1[key]}`);
-      return acc;
+      return [...acc, `- ${key}: ${parseJsonFromFile1[key]}`];
     }
-    acc.push(`+ ${key}: ${parseJsonFromFile2[key]}`);
-    return acc;
+    return [...acc, `+ ${key}: ${parseJsonFromFile2[key]}`];
   }, []);
   return `{\n${result.join('\n')}\n}`;
 };
