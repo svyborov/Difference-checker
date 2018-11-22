ad4b4f311af0041db21241963b4ad6209283a3ac
troyanec <sergey-vyborov@yandex.ru>
2018-11-22 16:59:22 +0300

    refactor parse, genDiff

--- src/genDiff.js
+++ src/genDiff.js
@@ -1,14 +1,28 @@
+import _ from 'lodash';
 import path from 'path';
-import { yamlParser, jsonParser, iniParser } from './parsers';
+import fs from 'fs';
+import parser from './parsers';
 
 const genDiff = (pathToFile1, pathToFile2) => {
   const extnameFile = path.extname(pathToFile1);
-  if (extnameFile === '.json') {
-    return jsonParser(pathToFile1, pathToFile2);
-  } if (extnameFile === '.yaml') {
-    return yamlParser(pathToFile1, pathToFile2);
-  }
-  return iniParser(pathToFile1, pathToFile2);
+  const dataToParse1 = fs.readFileSync(pathToFile1, 'utf-8');
+  const dataToParse2 = fs.readFileSync(pathToFile2, 'utf-8');
+  const parsedData1 = parser(dataToParse1, extnameFile);
+  const parsedData2 = parser(dataToParse2, extnameFile);
+  const uniqKeys = _.union(Object.keys(parsedData1), Object.keys(parsedData2));
+  const result = uniqKeys.reduce((acc, key) => {
+    if (_.has(parsedData1, key)) {
+      if (_.has(parsedData2, key)) {
+        if (parsedData1[key] === parsedData2[key]) {
+          return [...acc, `  ${key}: ${parsedData1[key]}`];
+        }
+        return [...acc, `- ${key}: ${parsedData1[key]}`, `+ ${key}: ${parsedData2[key]}`];
+      }
+      return [...acc, `- ${key}: ${parsedData1[key]}`];
+    }
+    return [...acc, `+ ${key}: ${parsedData2[key]}`];
+  }, []);
+  return `{\n${result.join('\n')}\n}`;
 };
 
 export default genDiff;
--- src/parsers.js
+++ src/parsers.js
@@ -1,40 +1,12 @@
-import fs from 'fs';
-import _ from 'lodash';
 import yaml from 'js-yaml';
 import ini from 'ini';
 
-const parser = (dataToDiff1, dataToDiff2) => {
-  const keysFromJsons = Object.keys(dataToDiff1).concat(Object.keys(dataToDiff2));
-  const uniqKeys = _.uniq(keysFromJsons);
-  const result = uniqKeys.reduce((acc, key) => {
-    if (_.has(dataToDiff1, key)) {
-      if (_.has(dataToDiff2, key)) {
-        if (dataToDiff1[key] === dataToDiff2[key]) {
-          return [...acc, `  ${key}: ${dataToDiff1[key]}`];
-        }
-        return [...acc, `- ${key}: ${dataToDiff1[key]}`, `+ ${key}: ${dataToDiff2[key]}`];
-      }
-      return [...acc, `- ${key}: ${dataToDiff1[key]}`];
-    }
-    return [...acc, `+ ${key}: ${dataToDiff2[key]}`];
-  }, []);
-  return `{\n${result.join('\n')}\n}`;
+const parsers = {
+  '.json': JSON.parse,
+  '.yaml': yaml.safeLoad,
+  '.ini': ini.parse,
 };
 
-export const yamlParser = (file1, file2) => {
-  const parseYamlFromFile1 = yaml.safeLoad(fs.readFileSync(file1, 'utf-8'));
-  const parseYamlFromFile2 = yaml.safeLoad(fs.readFileSync(file2, 'utf-8'));
-  return parser(parseYamlFromFile1, parseYamlFromFile2);
-};
+const parser = (data, format) => parsers[format](data);
 
-export const jsonParser = (file1, file2) => {
-  const parseJsonFromFile1 = JSON.parse(fs.readFileSync(file1, 'utf-8'));
-  const parseJsonFromFile2 = JSON.parse(fs.readFileSync(file2, 'utf-8'));
-  return parser(parseJsonFromFile1, parseJsonFromFile2);
-};
-
-export const iniParser = (file1, file2) => {
-  const parseIniFromFile1 = ini.parse(fs.readFileSync(file1, 'utf-8'));
-  const parseIniFromFile2 = ini.parse(fs.readFileSync(file2, 'utf-8'));
-  return parser(parseIniFromFile1, parseIniFromFile2);
-};
+export default parser;
