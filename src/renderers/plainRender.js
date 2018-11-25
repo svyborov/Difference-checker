26fefa2d8b4cf0665b20344d54cb8247296b7f83
troyanec <sergey-vyborov@yandex.ru>
2018-11-25 19:44:56 +0300

    refactor

--- src/renderers/plainRender.js
+++ src/renderers/plainRender.js
@@ -3,7 +3,7 @@ import _ from 'lodash';
 const stringify = value => (_.isObject(value) ? '[complex value]' : value);
 const typeActions = {
   unchanged: () => '',
-  nested: (data, indent, f) => `${f(data.children, (`${data.key}.`))}`,
+  nested: (data, parent, f) => `${f(data.children, (`${data.key}.`))}`,
   changed: (data, parent) => `${parent}${data.key} was updated. From ${stringify(data.valueBefore)} to ${stringify(data.valueAfter)}`,
   added: (data, parent) => `${parent}${data.key} was added with value: ${stringify(data.valueAfter)}`,
   deleted: (data, parent) => `${parent}${data.key} was removed`,
--- src/renderers/unevenRender.js
+++ src/renderers/unevenRender.js
@@ -1,29 +1,29 @@
 import _ from 'lodash';
 
-const stringify = (value, indent) => {
+const stringify = (value, level) => {
   if (!_.isObject(value)) {
     return (value);
   }
   const elementsOfValue = _.keys(value)
-    .reduce((acc, key) => [...acc, `${' '.repeat(indent * 4)}${key}: ${value[key]}`], []);
-  return ['{', ...elementsOfValue, `${' '.repeat((indent - 1) * 4)}}`].join('\n');
+    .reduce((acc, key) => [...acc, `${' '.repeat(level * 4)}${key}: ${value[key]}`], []);
+  return ['{', ...elementsOfValue, `${' '.repeat((level - 1) * 4)}}`].join('\n');
 };
 
 const typeActions = {
-  unchanged: (data, indent) => `${' '.repeat(indent * 4)}${data.key}: ${stringify(data.valueBefore, indent + 1)}`,
-  nested: (data, indent, f) => `${' '.repeat(indent * 4)}${data.key}: ${f(data.children, indent + 1)}`,
-  changed: (data, indent) => [`${' '.repeat(indent * 4 - 2)}- ${data.key}: ${stringify(data.valueBefore, indent + 1)}`,
-    `${' '.repeat(indent * 4 - 2)}+ ${data.key}: ${stringify(data.valueAfter, indent + 1)}`],
-  added: (data, indent) => `${' '.repeat(indent * 4 - 2)}+ ${data.key}: ${stringify(data.valueAfter, indent + 1)}`,
-  deleted: (data, indent) => `${' '.repeat(indent * 4 - 2)}- ${data.key}: ${stringify(data.valueBefore, indent + 1)}`,
+  unchanged: (data, level) => `${' '.repeat(level * 4)}${data.key}: ${stringify(data.valueBefore, level + 1)}`,
+  nested: (data, level, f) => `${' '.repeat(level * 4)}${data.key}: ${f(data.children, level + 1)}`,
+  changed: (data, level) => [`${' '.repeat(level * 4 - 2)}- ${data.key}: ${stringify(data.valueBefore, level + 1)}`,
+    `${' '.repeat(level * 4 - 2)}+ ${data.key}: ${stringify(data.valueAfter, level + 1)}`],
+  added: (data, level) => `${' '.repeat(level * 4 - 2)}+ ${data.key}: ${stringify(data.valueAfter, level + 1)}`,
+  deleted: (data, level) => `${' '.repeat(level * 4 - 2)}- ${data.key}: ${stringify(data.valueBefore, level + 1)}`,
 };
 
-const unevenRender = (data, indent = 1) => {
+const unevenRender = (data, level = 1) => {
   const renderedData = data.reduce((acc, value) => {
     const typeValue = value.type;
-    return [...acc, typeActions[typeValue](value, indent, unevenRender)];
+    return [...acc, typeActions[typeValue](value, level, unevenRender)];
   }, []);
-  return _.flatten(['{', ...renderedData, `${' '.repeat(indent * 4 - 4)}}`]).join('\n');
+  return _.flatten(['{', ...renderedData, `${' '.repeat(level * 4 - 4)}}`]).join('\n');
 };
 
 export default unevenRender;
